cf32d8fb928522293b1cfb2b155461d6c4866d97
ZW957 <58925682+ZW957@users.noreply.github.com>
2022-11-27 15:56:48 +0800

    人物+人物控制

--- Assets/myscript/playercontrollor.cs
+++ Assets/myscript/playercontrollor.cs
@@ -1,18 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontrollor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace player
+{ 
+    /// <summary>
+    /// player控制器
+    /// </summary>
+    public class playercontrollor : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
+
