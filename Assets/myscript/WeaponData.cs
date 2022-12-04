3dad617384f5214d809910a8b363b3602381ec21
ZW957 <58925682+ZW957@users.noreply.github.com>
2022-12-04 16:00:39 +0800

    武器初設定

---
+++ Assets/myscript/WeaponData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Weapon
+{
+    [CreateAssetMenu(menuName = "Weapon/Weapondata", fileName = " new Weapon data")]
+    public class WeaponData : ScriptableObject
+    {
+        [Header("武器攻擊力"),Range(0,100)]
+        public float attack;
+        [Header("武器數量速度生成位置")]
+        public WeaponObject[] weaponObjects;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+    [System.Serializable]
+    public class WeaponObject
+    {
+         [Header("武器速度")]
+         public Vector2 speed;
+         [Header("武器生成位置")]
+         public Vector3 pointSpawn;
+    }
+
+}
--- Assets/myscript/playercontrollor.cs
+++ Assets/myscript/playercontrollor.cs
@@ -9,6 +9,17 @@ namespace player
     /// </summary>
     public class playercontrollor : MonoBehaviour
     {
+        [SerializeField,Header("移動速度"),Range(0,100)]
+        private float speed = 3.5f;
+        private Animator playerani;
+        private Rigidbody2D playerrig;
+        private string playerWalk = "isRun";
+
+        private void Awake()
+        {
+            playerani = GetComponent<Animator>();
+            playerrig = GetComponent<Rigidbody2D>();
+        }
         // Start is called before the first frame update
         void Start()
         {
@@ -18,7 +29,26 @@ namespace player
         // Update is called once per frame
         void Update()
         {
-        
+            Move();
+        }
+        private void Move()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            print($"x軸:{x} y軸:{y}");
+            playerrig.velocity = new Vector2(x , y)* speed;
+            UpdateAnimaation(x, y);
+            Flip(x);
+        }
+
+        private void UpdateAnimaation(float x,float y)
+        {
+            playerani.SetBool(playerWalk, x != 0 || y != 0);
+        }
+        private void Flip(float x)
+        {
+            if (Mathf.Abs(x) < 0.1f) return;
+            transform.eulerAngles = new Vector2(0, x > 0 ? 0 : 180);
         }
     }
 }
